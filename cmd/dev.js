bd94549814de8334f12fc35bf4b74d2ca8c64fea
Nick Breaton <nick@breaton.com>
2015-09-29 14:11:00 -0400

    Fix development server error from removing node_modules location.

--- cmd/dev.js
+++ cmd/dev.js
@@ -10,13 +10,15 @@ var config = require("../lib/webpack");
 var project = require("../lib/project");
 
 module.exports = function () {
-  nodemon({
-    exec: path.join(__dirname, "..", "index.js") + " run --development",
-    ext: "js jsx json html",
-    watch: path.join(project.path.server, "..", "*")
+  clear(function () {
+    new WebpackDevServer(webpack(config("dev")), {
+      publicPath: config("dev").output.publicPath,
+      hot: true,
+      quiet: true
+    }).listen(8080, run);
   });
 
-  nodemon.on("start", function () {
+  function clear (cb) {
     var d = new Date(),
         h = d.getHours(),
         m = d.getMinutes(),
@@ -30,17 +32,18 @@ module.exports = function () {
       console.log(out + "---------------------------------------");
       console.log("[" + time + "] Starting development server!");
       console.log("---------------------------------------\n");
+
+      cb && cb();
     });
-  });
+  }
 
-  var conf = config("dev");
-  var server = new WebpackDevServer(webpack(conf), {
-    publicPath: conf.output.publicPath,
-    hot: true,
-    quiet: true
-  });
+  function run () {
+    nodemon({
+      exec: path.join(__dirname, "..", "index.js") + " run --development",
+      ext: "js jsx json html",
+      watch: path.join(project.path.server, "..", "*")
+    });
 
-  server.listen(8080, function (err) {
-    if (err) console.error(err);
-  });
+    nodemon.on("start", clear);
+  }
 }
